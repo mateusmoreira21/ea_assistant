563b4fcd44ad2f3b7f102a648e2284639ab9af07
matheusc3m <matheusfernandes3m@hotmail.com>
2020-06-09 16:20:16 -0300

    tela de qr criada

--- lib/app/modules/qrcode/qrcode/qrcode_page.dart
+++ lib/app/modules/qrcode/qrcode/qrcode_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'qrcode_controller.dart';
 
 class QrcodePage extends StatefulWidget {
@@ -10,18 +11,136 @@ class QrcodePage extends StatefulWidget {
   _QrcodePageState createState() => _QrcodePageState();
 }
 
+const flashOn = 'FLASH ON';
+const flashOff = 'FLASH OFF';
+const frontCamera = 'FRONT CAMERA';
+const backCamera = 'BACK CAMERA';
+
 class _QrcodePageState extends ModularState<QrcodePage, QrcodeController> {
+  var qrText = 'vazio';
+  var flashState = flashOn;
+  var cameraState = frontCamera;
+  QRViewController qrcontroller; //o nome desse qrcontroller era originalmente controller, mas tava dando conflito com o controler q
+  //ja tava aqui.
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   //use 'controller' variable to access controller
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          Expanded(
+            flex: 4,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+              overlay: QrScannerOverlayShape(
+                borderColor: Colors.red,
+                borderRadius: 10,
+                borderLength: 30,
+                borderWidth: 10,
+                cutOutSize: 300,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: FittedBox(
+              fit: BoxFit.contain,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.all(8),
+                        child: RaisedButton(
+                          onPressed: () {
+                            if (qrcontroller != null) {
+                              qrcontroller.toggleFlash();
+                              if (_isFlashOn(flashState)) {
+                                setState(() {
+                                  flashState = flashOff;
+                                });
+                              } else {
+                                setState(() {
+                                  flashState = flashOn;
+                                });
+                              }
+                            }
+                          },
+                          child:
+                              Text(flashState, style: TextStyle(fontSize: 20)),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.all(8),
+                        child: RaisedButton(
+                          onPressed: () {
+                            qrcontroller?.pauseCamera();
+                          },
+                          child: Text('Pausar', style: TextStyle(fontSize: 20)),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(8),
+                        child: RaisedButton(
+                          onPressed: () {
+                            qrcontroller?.resumeCamera();
+                          },
+                          child:
+                              Text('Resumir', style: TextStyle(fontSize: 20)),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(8),
+                        child: RaisedButton(
+                          onPressed: () {
+                            qrcontroller?.resumeCamera();
+                          },
+                          child: Text('Enviar', style: TextStyle(fontSize: 20)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
+  }
+
+  bool _isFlashOn(String current) {
+    return flashOn == current;
+  }
+
+  bool _isBackCamera(String current) {
+    return backCamera == current;
+  }
+
+  void _onQRViewCreated(QRViewController qrcontroller) {
+    this.qrcontroller = qrcontroller;
+    qrcontroller.scannedDataStream.listen((scanData) {
+      setState(() {
+        qrText = scanData;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    qrcontroller.dispose();
+    super.dispose();
   }
 }
